37886f23a7e2297b704f6af755e75b1e0a444828
Mehmet Efe <mehmetservanefe@gmail.com>
2025-01-01 01:54:53 +0300

    recruiter profile updates

---
+++ src/main/java/com/jobportal/controller/RecruiterProfileController.java
@@ -0,0 +1,86 @@
+package com.jobportal.controller;
+
+import com.jobportal.entity.RecruiterProfile;
+import com.jobportal.entity.User;
+import com.jobportal.repository.UserRepository;
+import com.jobportal.service.RecruiterProfileService;
+import com.jobportal.util.FileUploadUtil;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Objects;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/recruiter-profile")
+public class RecruiterProfileController {
+
+    private final RecruiterProfileService recruiterProfileService;
+    private final UserRepository userRepository;
+
+    public RecruiterProfileController(RecruiterProfileService recruiterProfileService, UserRepository userRepository) {
+        this.recruiterProfileService = recruiterProfileService;
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/")
+    public String recruiterProfile(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        if (authentication != null && authentication.isAuthenticated()) {
+//            String currentUserName = authentication.getName();
+//            System.out.println("Authenticated user: " + currentUserName);
+//        } else {
+//            System.out.println("User is not authenticated");
+//        }     DENEMEK İCİN CALISTIR
+
+        if(!(authentication instanceof AnonymousAuthenticationToken) ){
+            String currentUserName = authentication.getName();
+            User user = userRepository.findByEmail(currentUserName).orElseThrow(
+                    ()->new UsernameNotFoundException("User Not Found: "+currentUserName));
+            Optional<RecruiterProfile> recruiterProfile =
+                    recruiterProfileService.getRecruiterProfile(user.getUserId());
+            if(!recruiterProfile.isEmpty()){
+                model.addAttribute("profile", recruiterProfile.get());
+            }
+        }
+        return "recruiter_profile";
+    }
+
+    @PostMapping("/addNew")
+    public String addNew(RecruiterProfile recruiterProfile, @RequestParam("image") MultipartFile multipartfile,
+                         Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(!(authentication instanceof AnonymousAuthenticationToken) ){
+            String currentUserName = authentication.getName();
+            User user = userRepository.findByEmail(currentUserName).orElseThrow(
+                    ()->new UsernameNotFoundException("User Not Found: "+currentUserName));
+            recruiterProfile.setUserId(user);
+            recruiterProfile.setUserAccountId(user.getUserId());
+        }
+        model.addAttribute("profile", recruiterProfile);
+        String filename = "";
+        if(!multipartfile.getOriginalFilename().equals("")){
+            filename = StringUtils.cleanPath(Objects.requireNonNull(multipartfile.getOriginalFilename()));
+            recruiterProfile.setProfilePhoto(filename);
+        }
+        RecruiterProfile savedUser = recruiterProfileService.addNew(recruiterProfile);
+        String uploadDir = "photos/recruiter/" + savedUser.getUserAccountId();
+        try{
+            FileUploadUtil.saveFile(uploadDir, filename, multipartfile);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return "redirect:/dashboard/";
+    }
+}
